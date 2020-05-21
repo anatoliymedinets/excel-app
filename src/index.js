0d6ad82fc21c481b212b30f94d0de8c02f550b2a
anatoliymedinets <anatoliymedinets@users.noreply.github.com>
2020-05-22 01:31:45 +0300

    finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './scss/index.scss'
 
-console.log('Index')
+import { Excel } from './components/excel/Excel'
+import { Header } from '@/components/header/Header'
+import { Toolbar } from '@/components/toolbar/Toolbar'
+import { Formula } from '@/components/formula/Formula'
+import { Table } from '@/components/table/Table'
 
-async function f() {
-  await Promise.resolve()
-}
-
-f()
+const excel = new Excel('#app', {
+  components: [
+    Header, Toolbar, Formula, Table
+  ]
+})
+excel.render()
