d5b613c2cd967b83a9fda6d9f93667f5922fe64a
anatoliymedinets <anatoliymedinets@users.noreply.github.com>
2020-05-26 00:28:54 +0300

    finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -4,22 +4,31 @@ const CODES = {
   Z: 90
 }
 
-function createCell() {
+function createCell(_, index) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${index}"></div>
   `
 }
 
-function createCol(col) {
+function createCol(col, index) {
   return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
   `
 }
 
 function createRow(index, content) {
+  const resizer = index ? '<div class="row-resize" data-resize="row"></div>': ''
+  const dataTypeResizable = index ? 'data-type="resizable"' : ''
+
   return `
-  <div class="row">
-    <div class="row-info">${index ?? ''}</div>
+  <div class="row" ${dataTypeResizable}>
+    <div class="row-info">
+      ${index ?? ''}
+      ${resizer}
+    </div>
     <div class="row-data">${content}</div>
   </div>
   `
